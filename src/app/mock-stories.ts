01cfd88f64801d9562c4cf7fed3925bb9b68e5d5
Aaron Rohrbacher and Javier Jimenez <aaron@>
2017-10-31 14:39:59 -0700

    add funny buttons

--- src/app/mock-stories.ts
+++ src/app/mock-stories.ts
@@ -2,7 +2,9 @@ import { Story } from './story.model';
 
 
 export const STORIES: Story[] = [
-  new Story("Banannas or Crack", "You have 2 dollars to your name. You enter the grocery store, and are craving a bananna. Your crack high is also wearing off. Do you buy banannas, or visit the nice gentleman outstide for a quick hit, and then steal the banannas later?", 3, 1),
+  new Story("Banannas or Crack", "You have 2 dollars to your name. You enter the grocery store, and are craving a bananna. Your crack high is also wearing off. Do you buy banannas, or visit the nice gentleman outstide for a quick hit, and then steal the banannas later?", 3, 'Crack', 'Bananna', 1),
 
-  new Story("Mark or Dawson", "Mark gave you a hit of crack for your $2, and now you're not even hungry for the banannas. But man, you're ready for some sweet action! Mark said he can hook you up with a crack whore down the street, but you've been invited by Dawson to play ulitmate frisbee at the park. What to do?", 4, 2)
+  new Story("Mark or Dawson", "Mark gave you a hit of crack for your $2, and now you're not even hungry for the banannas. But man, you're ready for some sweet action! Mark said he can hook you up with a crack whore down the street, but you've been invited by Dawson to play ulitmate frisbee at the park. What to do?", 4, 'CrackWhore', 'Frisbee', 2)
+
+  
 ]
--- src/app/story.model.ts
+++ src/app/story.model.ts
@@ -1,3 +1,3 @@
 export class Story {
-  constructor(public title: string, public story: string, public points: number, public id: number) {}
+  constructor(public title: string, public story: string, public points: number, public positive: string, public negative: string, public id: number) {}
 }
